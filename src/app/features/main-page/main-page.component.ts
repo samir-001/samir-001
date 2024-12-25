a25705a7e9d7c89c66c0665c81f05cfaaebff16f
samir <samir.harg@yahoo.com>
2024-12-25 21:00:36 +0200

    add skills ,testemonils and client

--- src/app/features/main-page/main-page.component.ts
+++ src/app/features/main-page/main-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MainSectionComponent } from './components/main-section/main-section.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { ClientsComponent } from './components/clients/clients.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [MainSectionComponent],
+  imports: [MainSectionComponent,SkillsComponent,TestimonialComponent,ClientsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
